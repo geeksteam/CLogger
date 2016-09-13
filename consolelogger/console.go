fed9232c2e8bc6e5fb906d61446cdd676b75f48f
Nikita <nikita.bokovoy@gmail.com>
2016-09-13 15:29:14 +0300

    Initial commit

---
+++ consolelogger/console.go
@@ -0,0 +1,64 @@
+package consolelogger
+
+import (
+	"log"
+	"os"
+
+	"github.com/geeksteam/klogger"
+)
+
+type (
+	consoleLogger struct {
+		debug    *log.Logger
+		info     *log.Logger
+		warning  *log.Logger
+		error    *log.Logger
+		critical *log.Logger
+	}
+)
+
+const (
+	CLR_0 = "\x1b[30;1m"
+	CLR_R = "\x1b[31;1m"
+	CLR_G = "\x1b[32;1m"
+	CLR_Y = "\x1b[33;1m"
+	CLR_B = "\x1b[34;1m"
+	CLR_M = "\x1b[35;1m"
+	CLR_C = "\x1b[36;1m"
+	CLR_W = "\x1b[37;1m"
+	CLR_N = "\x1b[0m"
+)
+
+func NewConsoleLogger() klogger.Logger {
+	return &consoleLogger{
+		log.New(os.Stdout, CLR_0, log.Ldate|log.Ltime),
+		log.New(os.Stdout, CLR_G, log.Ldate|log.Ltime),
+		log.New(os.Stdout, CLR_Y, log.Ldate|log.Ltime),
+		log.New(os.Stdout, CLR_R, log.Ldate|log.Ltime),
+		log.New(os.Stdout, CLR_C, log.Ldate|log.Ltime),
+	}
+}
+
+func (logger *consoleLogger) Debug(format string, args ...interface{}) {
+	logger.debug.Printf("DEBUG: "+format+CLR_N, args...)
+}
+
+func (logger *consoleLogger) Info(format string, args ...interface{}) {
+	logger.info.Printf("INFO:  "+format+CLR_N, args...)
+}
+
+func (logger *consoleLogger) Warning(format string, args ...interface{}) {
+	logger.warning.Printf("WARN:  "+format+CLR_N, args...)
+}
+
+func (logger *consoleLogger) Error(format string, args ...interface{}) {
+	logger.error.Printf("ERROR: "+format+CLR_N, args...)
+}
+
+func (logger *consoleLogger) Critical(format string, args ...interface{}) {
+	logger.critical.Printf("FATAL: "+format+CLR_N, args...)
+}
+
+func init() {
+	klogger.New = NewConsoleLogger
+}
---
+++ klogger.go
@@ -0,0 +1,17 @@
+package klogger
+
+type (
+	Logger interface {
+		Debug(format string, args ...interface{})
+		Info(format string, args ...interface{})
+		Warning(format string, args ...interface{})
+		Error(format string, args ...interface{})
+		Critical(format string, args ...interface{})
+	}
+
+	loggerFactory func() Logger
+)
+
+var (
+	New loggerFactory
+)
